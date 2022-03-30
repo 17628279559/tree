5abd08fbcf6f6c05ef335a5ab4ba21317c77be93
snaketao <z17628279559@163.com>
2022-03-30 21:12:31 +0800

    3.30大更

--- src/views/pieChart/pie.js
+++ src/views/pieChart/pie.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-let constcolor = ["#FFFF33", "#FFFF00", "#CCFF33", "#BBFF00", "#99FF33", "#77FF00", "#FF3333", "#FF0000", "#FF44AA", "#FF0088", "#33FF33", "#00FF00", "#33FFAA", "#00FF99", "#33FFDD", "#00FFCC", "#33FFFF", "#00FFFF", "#33CCFF", "#00BBFF", "#5599FF", "#0066FF", "#5555FF", "#0000FF", "#7744FF", "#5500FF", "#9955FF", "#7700FF", "#B94FFF", "#9900FF", "#E93EFF", "#CC00FF", "#FF3EFF", "#FF00FF"]
+let const_color = ['#FFFF33', '#FFFF00', '#CCFF33', '#BBFF00', '#99FF33', '#77FF00', '#FF3333', '#FF0000', '#FF44AA', '#FF0088', '#33FF33', '#00FF00', '#33FFAA', '#00FF99', '#33FFDD', '#00FFCC', '#33FFFF', '#00FFFF', '#33CCFF', '#00BBFF', '#5599FF', '#0066FF', '#5555FF', '#0000FF', '#7744FF', '#5500FF', '#9955FF', '#7700FF', '#B94FFF', '#9900FF', '#E93EFF', '#CC00FF', '#FF3EFF', '#FF00FF']
 
 Array.prototype.shuffle = function () {
     let array = this;
@@ -10,72 +10,151 @@ Array.prototype.shuffle = function () {
     }
 }
 
-let pie = (id, svgRef, data, options, title) => {
+
+let pie = (id, svgRef, data, options) => {
     let svg = d3.select(svgRef.value);
-    constcolor.shuffle();
-    const chart_width = 550,
-        chart_height = 550,
-        PI = 3.1415926;
 
-
-    let chart_size = d3.min([chart_width, chart_height]),
-        colorScale = d3.scaleLinear().domain([0, data.length / 3, data.length * 2 / 3, data.length]).range(['#4158D0', '#C850C0', '#FFCC70', "#FF0000"]),
-        donut, piee, parameter, yScale, new_g,
-        data_size = 0;
+    const_color.shuffle();
 
     let settings = {
+        title: "",
+        chart_height: 550,
+        show_lable_text: true,
+        raised: 20,
+        chart_height: 550,
+        text_anchor: "middle",
+        pad_angle: 0,
+        introduce_backguound_color: '#8EC5FC',
+        is_rotate: false,
+        stroke_width: 0,
+        stroke: '#fff',
         radius_inner: 0,
-        radius_outer: chart_size * (2 / 5),
-        radius_label: chart_size * (2 / 5) + 20,
-        useConstcolor: false,
+        radius_outer: 220,
+        radius_label: 240,
+        use_const_color: true,
         label_margin: 10,
         group_data: 0,
+        lable_text_size: "1.1em",
+        gradient_color: ['#4158D0', '#C850C0', '#FFCC70', '#4158D0'],
         name: item => item.name,
-        getvalue: item => item.value,
+        get_value: item => item.value,
         color: (item, index) => colorScale(index)
     };
-    data.forEach(e => {
-        data_size += settings.getvalue(e)
-    });
 
     // 更新settings
     for (parameter in options)
         settings[parameter] = options[parameter];
 
 
+    const chart_size = d3.min([settings.chart_width, settings.chart_height]),
+        PI = 3.1415926;
+
+    let domain_gradient_color = []
+    for (let i = 0; i < settings.gradient_color.length; i++) {
+        domain_gradient_color.push(data.length * (i / (settings.gradient_color.length - 1)))
+    }
+    console.log(data.length);
+    let colorScale = d3.scaleLinear().domain(domain_gradient_color).range(settings.gradient_color),
+        donut, piee, parameter, scale, introduce_g,
+        data_size = 0;
+
+
+    data.forEach(e => {
+        data_size += settings.get_value(e)
+    });
+
+
+
+
     // 合并data中占比小于group_data的元素
     const group_data = data => {
         let removed_data_size = 0;
 
-        for (let i = data.length - 1; i >= 0; i--)
-            if ((settings.getvalue(data[i]) / data_size) * 100 < settings.group_data)
-                removed_data_size += settings.getvalue(data.splice(i, 1)[0]);
+        for (let i = data.length - 1; i >= 0; i--) {
+            if (settings.get_value(data[i]) === 0) {
+                data.splice(i, 1);
+            }
+            else if ((settings.get_value(data[i]) / data_size) * 100 < settings.group_data) {
+                removed_data_size += settings.get_value(data.splice(i, 1)[0]);
+            }
+        }
+
 
         if (removed_data_size > 0)
             data.push({ index: 35, name: 'Other', value: removed_data_size });
         return data;
     };
 
-    if (settings.group_data > 0)
-        data = group_data(data);
+
+    data = group_data(data);
 
 
     let nowAngle = 0;
     for (let i = 0; i < data.length; i++) {
-        data[i]['lableAngle'] = nowAngle + ((settings.getvalue(data[i]) / data_size) * 100 / 100) * PI;
-        nowAngle += ((settings.getvalue(data[i]) / data_size) * 100 / 100) * PI * 2;
+        data[i]['lableAngle'] = nowAngle + ((settings.get_value(data[i]) / data_size) * 100 / 100) * PI;
+        nowAngle += ((settings.get_value(data[i]) / data_size) * 100 / 100) * PI * 2;
     }
 
+    const text_over = item => {
+        if (settings.is_rotate) {
+            return `translate(0,5) rotate(${(item.lableAngle - PI / 2) * (180 / PI)}) translate(${settings.radius_label - 10 + settings.raised},0)`
+        } else return `translate(${Math.sin(item.lableAngle) * (settings.radius_label + settings.raised)},${-(Math.cos(item.lableAngle) * (settings.radius_label + settings.raised))})`;
+    };
 
+    const text_out = item => {
+        if (settings.is_rotate) {
+            return `translate(0,5) rotate(${(item.lableAngle - PI / 2) * (180 / PI)}) translate(${settings.radius_label - 10},0)`
+        } else return `translate(${Math.sin(item.lableAngle) * settings.radius_label},${-(Math.cos(item.lableAngle) * settings.radius_label) + 8})`
+    };
+
+    const over = (item, index) => {
+        d3.select(`#_${id}_path${index.data.index}`)
+            .transition()
+            .duration(50)
+            .attr('fill-opacity', 0.5)
+            .attr('d', window.selectArc[`arc${id}`]);
+        d3.select(`#_${id}_text${index.data.index}`)
+            .transition()
+            .duration(50)
+            .text(index.data.value)
+            .attr('transform', text_over);
+        d3.select(`#_${id}_introduce${index.data.index}`)
+            .transition()
+            .duration(50)
+            .style('fill', settings.introduce_backguound_color);
+    };
+
+    const out = (item, index) => {
+        d3.select(`#_${id}_path${index.data.index}`)
+            .transition()
+            .duration(200)
+            .attr('fill-opacity', 1)
+            .attr('d', window.arc[`arc${id}`]);
+        d3.select(`#_${id}_text${index.data.index}`)
+            .transition()
+            .duration(50)
+            .text(index.data.name)
+            .attr('transform', text_out);
+        d3.select(`#_${id}_introduce${index.data.index}`)
+            .transition()
+            .duration(50)
+            .style('fill', '#f5f5f5');
+    };
+
+    const introduce_rect_fillcolor = (item, index) => {
+        if (settings.use_const_color)
+            return const_color[item.index];
+        else return settings.color(item.data, index);
+    }
 
     donut = svg
         .append('g')
         .attr('class', 'donut')
         .attr('transform', `translate(${chart_size / 2},${chart_size / 2})`);
 
-    new_g = svg
+    introduce_g = svg
         .append('g')
-        .attr('class', 'new_g')
+        .attr('class', 'introduce_g')
         .attr('transform', `translate(${chart_size},20)`);
 
     window.arc[`arc${id}`] = d3.arc()
@@ -84,10 +163,11 @@ let pie = (id, svgRef, data, options, title) => {
 
     window.selectArc[`arc${id}`] = d3.arc()
         .innerRadius(settings.radius_inner)
-        .outerRadius(settings.radius_outer + 20);
+        .outerRadius(settings.radius_outer + settings.raised);
 
     piee = d3.pie()
-        .value(settings.getvalue)
+        .value(settings.get_value)
+        .padAngle(settings.pad_angle)
         .sort((a, b) => a.index - b.index)(data);
 
 
@@ -96,135 +176,73 @@ let pie = (id, svgRef, data, options, title) => {
         .enter()
         .append('path')
         .attr('id', item => `_${id}_path${item.data.index}`)
-        .style('fill', (item, index) => {
-            if (!settings.useConstcolor)
-                return constcolor[item.index];
-            else return settings.color(item.data, index);
-        })
+        .style('fill', introduce_rect_fillcolor)
         .attr('d', window.arc[`arc${id}`])
-        .on('mouseover', (item, index) => {
-            d3.select(`#_${id}_path${index.data.index}`)
-                .transition()
-                .duration(50)
-                .attr('fill-opacity', 0.5)
-                .attr('d', window.selectArc[`arc${id}`]);
-            d3.select(`#_${id}_text${index.data.index}`)
-                .transition()
-                .duration(50)
-                .text(index.data.value)
-                .attr('transform', `translate(${Math.sin(index.data.lableAngle) * (settings.radius_label + 20)},${-(Math.cos(index.data.lableAngle) * (settings.radius_label + 20))})`);
-            d3.select(`#_${id}_inter${index.data.index}`)
-                .transition()
-                .duration(50)
-                .attr('fill-opacity', 0.5);
-        })
-        .on('mouseout', (item, index) => {
-            d3.select(`#_${id}_path${index.data.index}`)
-                .transition()
-                .duration(200)
-                .attr('fill-opacity', 1)
-                .attr('d', window.arc[`arc${id}`]);
-            d3.select(`#_${id}_text${index.data.index}`)
-                .transition()
-                .duration(50)
-                .text(index.data.name)
-                .attr('transform', item => `translate(${Math.sin(item.lableAngle) * settings.radius_label},${-(Math.cos(item.lableAngle) * settings.radius_label) + 8})`);
-            d3.select(`#_${id}_inter${index.data.index}`)
-                .transition()
-                .duration(50)
-                .attr('fill-opacity', 1);
-        });
+        .attr('stroke-width', settings.stroke_width)
+        .attr('stroke', settings.stroke)
+        .on('mouseover', over)
+        .on('mouseout', out);
 
-    donut.selectAll('text')
-        .data(data)
-        .enter()
-        .append('text')
-        .text(item => item.name)
-        .attr('id', item => `_${id}_text${item.index}`)
-        .attr('transform', item => `translate(${Math.sin(item.lableAngle) * settings.radius_label},${-(Math.cos(item.lableAngle) * settings.radius_label) + 8})`)
-        .attr('text-anchor', 'middle')
-        .attr('fill', 'rgb(90,90,90)');
+    if (settings.show_lable_text) {
+        donut.selectAll('text')
+            .data(data)
+            .enter()
+            .append('text')
+            .text(item => item.name)
+            .attr('id', item => `_${id}_text${item.index}`)
+            .attr('transform', text_out)
+            .attr('font-size', settings.lable_text_size)
+            .attr('text-anchor', settings.text_anchor)
+            .attr('fill', 'rgb(90,90,90)');
 
-    donut.append('text')
-        .text(title)
-        .attr('transform', `translate(0,${settings.radius_label + 25})`)
-        .attr('font-size', `1.3em`)
-        .attr('text-anchor', 'middle');
+        donut.append('text')
+            .text(settings.title)
+            .attr('transform', `translate(0,${settings.radius_label + 25})`)
+            .attr('font-size', `1.3em`)
+            .attr('text-anchor', 'middle');
 
-    yScale = d3.scaleLinear()
-        .domain([0, 12])
-        .range([0, 510])
+        scale = d3.scaleLinear()
+            .domain([0, 12])
+            .range([0, 510])
 
-    new_g.selectAll('.inter')
-        .data(piee)
-        .enter()
-        .append('rect')
-        .attr('class', 'inter')
-        .attr('id', item => `_${id}_inter${item.data.index}`)
-        .attr('width', 100)
-        .attr('height', 510 / 12)
-        .attr('transform', item => `translate(${(item.index % 4) * 100},${yScale(Math.floor(item.index / 4))})`)
-        .style('fill', '#f5f5f5')
-        .on('mouseover', (item, index) => {
-            d3.select(`#_${id}_path${index.data.index}`)
-                .transition()
-                .duration(50)
-                .attr('fill-opacity', 0.5)
-                .attr('d', window.selectArc[`arc${id}`]);
-            d3.select(`#_${id}_text${index.data.index}`)
-                .transition()
-                .duration(50)
-                .text(index.data.value)
-                .attr('transform', `translate(${Math.sin(index.data.lableAngle) * (settings.radius_label + 20)},${-(Math.cos(index.data.lableAngle) * (settings.radius_label + 20))})`);
-            d3.select(`#_${id}_inter${index.data.index}`)
-                .transition()
-                .duration(50)
-                .style('fill', '#8EC5FC');
-        })
-        .on('mouseout', (item, index) => {
-            d3.select(`#_${id}_path${index.data.index}`)
-                .transition()
-                .duration(200)
-                .attr('fill-opacity', 1)
-                .attr('d', window.arc[`arc${id}`]);
-            d3.select(`#_${id}_text${index.data.index}`)
-                .transition()
-                .duration(50)
-                .text(index.data.name)
-                .attr('transform', item => `translate(${Math.sin(item.lableAngle) * settings.radius_label},${-(Math.cos(item.lableAngle) * settings.radius_label) + 8})`);
-            d3.select(`#_${id}_inter${index.data.index}`)
-                .transition()
-                .duration(50)
-                .style('fill', '#f5f5f5');
-        });
+        introduce_g.selectAll('.introduce')
+            .data(piee)
+            .enter()
+            .append('rect')
+            .attr('class', 'introduce')
+            .attr('id', item => `_${id}_introduce${item.data.index}`)
+            .attr('width', 100)
+            .attr('height', 510 / 12)
+            .attr('transform', item => `translate(${(item.index % 4) * 100},${scale(Math.floor(item.index / 4))})`)
+            .style('fill', '#f5f5f5')
+            .on('mouseover', over)
+            .on('mouseout', out);
 
 
 
-    new_g.selectAll('.inter_rect')
-        .data(piee)
-        .enter()
-        .append('rect')
-        .attr('class', 'inter_rect')
-        .attr('width', 22)
-        .attr('height', 22)
-        .attr('transform', item => `translate(${(item.index % 4) * 100 + 10.25},${yScale(Math.floor(item.index / 4)) + 10.25})`)
-        .style('fill', (item, index) => {
-            if (!settings.useConstcolor)
-                return constcolor[item.index];
-            else return settings.color(item.data, index);
-        })
+        introduce_g.selectAll('.introduce_rect')
+            .data(piee)
+            .enter()
+            .append('rect')
+            .attr('class', 'introduce_rect')
+            .attr('width', 22)
+            .attr('height', 22)
+            .attr('transform', item => `translate(${(item.index % 4) * 100 + 10.25},${scale(Math.floor(item.index / 4)) + 10.25})`)
+            .style('fill', introduce_rect_fillcolor)
+            .on('mouseover', over)
+            .on('mouseout', out);
 
-    new_g.selectAll('.inter_text')
-        .data(piee)
-        .enter()
-        .append('text')
-        .attr('class', 'inter_text')
-        .text(item => item.data.name)
-        .attr('transform', item => `translate(${(item.index % 4) * 100 + 10.25 + 32},${yScale(Math.floor(item.index / 4)) + 10.25 + 17})`)
-        .style('font-size', '1em')
-
-
-    return svg;
+        introduce_g.selectAll('.introduce_text')
+            .data(piee)
+            .enter()
+            .append('text')
+            .attr('class', 'introduce_text')
+            .text(item => item.data.name)
+            .attr('transform', item => `translate(${(item.index % 4) * 100 + 10.25 + 32},${scale(Math.floor(item.index / 4)) + 10.25 + 17})`)
+            .attr('font-size', settings.lable_text_size)
+            .on('mouseover', over)
+            .on('mouseout', out);
+    }
 };
 
 export default pie;
--- src/views/pieChart/piechart.vue
+++ src/views/pieChart/piechart.vue
@@ -3,6 +3,36 @@ import { ref, onMounted } from 'vue';
 import Axios from 'axios';
 import pie from './pie.js'
 
+let co2 = [{ name: "Road", value: 11.9 },
+{ index:1,name: "Aviation", value: 1.9 },
+{ index:2,name: "Rail", value: 0.4 },
+{ index:3,name: "Pipeline", value: 0.3 },
+{ index:4,name: "Ship", value: 1.7 },
+{ index:5,name: "Residential", value: 10.9 },
+{ index:6,name: "Commercial", value: 6.6 },
+{ index:7,name: "Iron&Steel", value: 7.2 },
+{ index:8,name: "Non-ferousmetals", value: 0.7 },
+{ index:9,name: "Machinery", value: 0.5 },
+{ index:10,name: "Foodandtobacco", value: 1 },
+{ index:11,name: "Paper,pulp&printing", value: 0.6 },
+{ index:12,name: "Chemical&petrochemical(energy)", value: 3.6 },
+{ index:13,name: "Otherindustry", value: 10.6 },
+{ index:14,name: "EnergyinAgri&Fishing", value: 1.7 },
+{ index:15,name: "Unallocatedfuelcombustion", value: 7.8 },
+{ index:16,name: "Coal", value: 1.9 },
+{ index:17,name: "Oil&NaturalGas", value: 3.9 },
+{ index:18,name: "Cement", value: 3 },
+{ index:19,name: "Chemical&petrochemical(industrial)", value: 2.2 },
+{ index:20,name: "Livestock&Manure", value: 5.8 },
+{ index:21,name: "RiceCultivation", value: 1.3 },
+{ index:22,name: "AgriculturalSoils", value: 4.1 },
+{ index:23,name: "CropBurning", value: 3.5 },
+{ index:24,name: "ForestLand", value: 2.2 },
+{ index:25,name: "Cropland", value: 1.4 },
+{ index:26,name: "Grassland", value: 0.1 },
+{ index:27,name: "Landfills", value: 1.9 },
+{ index:28,name: "Wastewater", value: 1.3 }]
+
 
 window.arc = {};
 window.selectArc = {};
@@ -10,6 +40,8 @@ const svgRef1 = ref(null);
 const svgRef2 = ref(null);
 const svgRef3 = ref(null);
 const svgRef4 = ref(null);
+const svgRef5 = ref(null);
+const svgRef6 = ref(null);
 const getdataApi = "/api/getcovie"
 
 let get_random_data = limit => {
@@ -41,31 +73,78 @@ const get_confirm = (data,field) => {
 
 
 const pie1 = {
+  title:"各省今日新增",
   group_data: 0,
   radius_inner: 0,
 }
 
 const pie2 = {
+  title:"各省总数据",
   group_data: 0.2,
   radius_inner: 150
 }
 const pie3 = {
+  title:"随机渐变",
   group_data: 0,
   radius_inner: 0,
-  useConstcolor: true
+  use_const_color: false,
+  text_anchor:"left",
+  is_rotate:true
 }
 const pie4 = {
+  title:"随机圆环",
+  padAngle: 0.005,
   group_data: 0,
-  radius_inner: 190
+  radius_inner: 180,
+  is_rotate:true,
+  stroke_width :3,
+  text_anchor:"left",
+  stroke : "#DCF"
 }
+
+const pie5 = {
+  show_lable_text:false,
+  use_const_color: false,
+  radius_outer: 160,
+  gradient_color:["#3EECAC","#EE74E7","#FA8BFF","#74EBD5","#3EECAC"]
+}
+
+const pie6 = {
+  title:"双重圆环",
+  padAngle: 0.005,
+  group_data: 0,
+  use_const_color: false,
+  radius_inner: 190,
+  is_rotate:true,
+  text_anchor:"left",
+  gradient_color:["#3EECAC","#EE74E7","#FA8BFF","#74EBD5","#3EECAC"]
+}
+
+const pie7 = {
+  title:"二氧化碳排放量占比",
+  padAngle: 0.005,
+  group_data: 0,
+  radius_inner: 100,
+  radius_outer:200,
+  lable_text_size:"0.8em",
+  radius_label:220,
+  is_rotate:true,
+  text_anchor:"left"
+}
+
+
+const constdata = get_random_data(40)
 
 onMounted(() => {
   Axios.get(getdataApi).then(response => {
     data = response.data;
-    pie(1, svgRef1, get_confirm(data,'today_confirm'), pie1,"各省今日新增");
-    pie(2, svgRef2, get_confirm(data,'total_confirm'), pie2,"各省总数据");
-    pie(3, svgRef3, get_random_data(48), pie3,"随机渐变");
-    pie(4, svgRef4, get_random_data(17), pie4,"随机圆环");
+    pie(1, svgRef1, get_confirm(data,'today_confirm'), pie1);
+    pie(2, svgRef2, get_confirm(data,'total_confirm'), pie2);
+    pie(3, svgRef3, get_random_data(30), pie3);
+    pie(4, svgRef4, get_random_data(17), pie4);
+    pie(5, svgRef5, constdata, pie5);
+    pie(6, svgRef5, constdata, pie6);
+    pie(7,svgRef6,co2,pie7);
   });
 });
 
@@ -76,6 +155,9 @@ onMounted(() => {
 
 <template>
   <div id="all">
+    <h1>中国各省疫情实时饼图</h1>
+    <h3>赵文韬-2019302120066</h3>
+    <div style="clear:both"></div>
     <svg class="center"
          width=1000
          height=550
@@ -89,6 +171,11 @@ onMounted(() => {
     <svg class="center"
          width=1000
          height=550
+         ref="svgRef6">
+    </svg>
+    <svg class="center"
+         width=1000
+         height=550
          ref="svgRef3">
     </svg>
     <svg class="center"
@@ -96,11 +183,23 @@ onMounted(() => {
          height=550
          ref="svgRef4">
     </svg>
+    <svg class="center"
+         width=1000
+         height=550
+         ref="svgRef5">
+    </svg>
   </div>
 
 </template>
 
 <style scoped>
+h1{
+    text-align: center;
+}
+h3{
+    margin-right: 160px;
+    float: right;
+}
 #all {
   width: 100%;
   height: 100%;
